fea38b055f734192d0d66b42cea9af2e70b6a22e
maliikabdi <lildarwish13158@gmail.com>
2024-09-23 12:32:57 +0300

    completed lab

---
+++ css/js intro.js/intro.js
@@ -0,0 +1,25 @@
+let name = "abdi"
+let age = 35
+
+console.log( name, age);
+
+
+
+let x = 20
+let y = 40
+let z = 30
+
+
+console.log(x+y+z);
+console.log (x, " + ", y, " + ", z," = " ,  x+y+z);
+console.log(`${x}+${y}+${z}=${x+y+z}`);
+
+// arithmetic operators
+// +, -, /,*, %/perform remainder,  **
+ console.log(2 % 3);
+
+ console.log (4 ** 5);
+
+ console.log(12 +5-12/6*(7+9));
+ 
+ console.log(99/20);
